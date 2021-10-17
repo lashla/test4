042aae7292c1c0cf6702f9ac4ced0d73325ca25f
lashla <45652713+lashla@users.noreply.github.com>
2021-10-17 17:43:44 +0300

    Create main.kt

---
+++ main.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val fullStr: String = "NAME=\"English\", TYPE=AUDIO, GROUP-ID=\"audio-stereo-64\", LANGUAGE=\"en\", DEFAULT=YES, AUTOSELECT=YES, URI=\"english.m3u8\""
+    val newStr: List<String> = fullStr.split(", ")
+    for (index in newStr.indices) {
+        println(newStr[index])
+    }
+}
