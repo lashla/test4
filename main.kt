d239d10f7a5c2c3e0e88c2f37e6489aa35c4bd59
lashla <45652713+lashla@users.noreply.github.com>
2021-10-17 18:49:10 +0300

    Update main.kt

--- main.kt
+++ main.kt
@@ -2,7 +2,11 @@ fun main() {
     val fullStr: String =
         "NAME=\"English\", TYPE=AUDIO, GROUP-ID=\"audio-stereo-64\", LANGUAGE=\"en\", DEFAULT=YES, AUTOSELECT=YES, URI=\"english.m3u8\""
     val newStr: List<String> = fullStr.split(", ")
+    var key: String
+    var value: String
     for (index in newStr.indices) {
-        println("KEY-${newStr[index].replace("=", " value-").replace("\"", "")}")
+       key = newStr[index].substringBefore("=")
+        value = newStr[index].substringAfter("=").removeSurrounding("\"")
+        println("key-$key value-$value")
     }
 }
